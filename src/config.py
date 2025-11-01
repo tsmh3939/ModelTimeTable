a5acfcb0838947df34998bf50fa38e459bf90707
tsmh3939 <kingsfort327@gmail.com>
2025-11-01 15:20:53 +0900

    テーマの変更・カスタマイズに対応

--- src/config.py
+++ src/config.py
@@ -1,2 +1,37 @@
 APP_NAME = "ModelTimeTable"
+DEFAULT_THEME_NAME = "light"
 DEBUG = True
+
+# 利用可能なdaisyUIテーマのリスト
+AVAILABLE_THEMES = [
+    "light",
+    "dark",
+    "dim",
+    "cupcake",
+    "bumblebee",
+    "emerald",
+    "corporate",
+    "synthwave",
+    "retro",
+    "cyberpunk",
+    "valentine",
+    "halloween",
+    "garden",
+    "forest",
+    "aqua",
+    "lofi",
+    "pastel",
+    "fantasy",
+    "wireframe",
+    "black",
+    "luxury",
+    "dracula",
+    "cmyk",
+    "autumn",
+    "business",
+    "acid",
+    "lemonade",
+    "night",
+    "coffee",
+    "winter",
+]
