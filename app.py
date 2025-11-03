f3595da43d53be788ff78d8810e1eedb21afd139
tsmh3939 <kingsfort327@gmail.com>
2025-11-03 21:52:08 +0900

    デフォルトのポート番号を変更

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 if __name__ == '__main__':
     # Docker環境では0.0.0.0にバインドする必要がある
     host = os.environ.get('FLASK_HOST', '0.0.0.0')
-    port = int(os.environ.get('FLASK_PORT', 5000))
+    port = int(os.environ.get('PORT', os.environ.get('FLASK_PORT', 8080)))
     debug = os.environ.get('FLASK_DEBUG', 'true').lower() == 'true'
 
     app.run(host=host, port=port, debug=debug)
